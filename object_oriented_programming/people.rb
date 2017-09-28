6b9c8206a041a21b4198681e412e9283668533e4
Abby Christens <achristens@gmail.com>
2017-09-28 13:25:18 -0400

    Adding Person class and initialize

--- object_oriented_programming/people.rb
+++ object_oriented_programming/people.rb
@@ -1,4 +1,18 @@
-class Student
+class Person
+
+  def initialize(name)
+    @name = name
+  end
+
+  # Person readers
+  def name
+    @name
+  end
+  
+end
+
+
+class Student < Person
 
   def learn
     "I get it!"
@@ -6,5 +20,18 @@ class Student
 
 end
 
-class Instructor
+
+class Instructor < Person
+
+  def teach
+    "Everything in Ruby is an Object"
+  end
+
 end
+
+prof = Instructor.new
+stud = Student.new
+
+
+puts prof.teach
+puts stud.learn
