75d81a6f15a64dc6b1ceabebb166373ae9a6eea2
Abby Christens <achristens@gmail.com>
2017-09-28 13:30:34 -0400

    Adding common greeting

--- object_oriented_programming/people.rb
+++ object_oriented_programming/people.rb
@@ -4,18 +4,30 @@ class Person
     @name = name
   end
 
+  def greeting
+    "Hi, my name is #{name}."
+  end
+
   # Person readers
   def name
     @name
   end
-  
+
 end
 
 
 class Student < Person
 
+  def initialize(name)
+    super(name)
+  end
+
   def learn
     "I get it!"
+  end
+
+  def greeting
+    super
   end
 
 end
@@ -23,15 +35,22 @@ end
 
 class Instructor < Person
 
+  def initialize(name)
+    super(name)
+  end
+
   def teach
     "Everything in Ruby is an Object"
   end
 
+  def greeting
+    super
+  end
+
 end
 
-prof = Instructor.new
-stud = Student.new
+prof = Instructor.new("Albert Einstein")
+stud = Student.new("Abby Christens")
 
-
-puts prof.teach
-puts stud.learn
+puts stud.greeting
+puts prof.greeting
